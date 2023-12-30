8bc3b1928ed43121e75b75760694da8519729f61
AnkitJagtap01 <91031017+AnkitJagtap01@users.noreply.github.com>
2023-12-30 05:18:50 +0000

    New features

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 let secretNumber = generateSecretNumber();
 let attempts = 0;
-let timer, startTime;
+let timer, timeRemaining = 10 * 60; // 10 minutes in seconds
 let timerRunning = false;
-let previousGuesses = new Set(); // Store previous guesses
-
-function enableGameControls(enable) {
-    document.getElementById("pause-button").disabled = !enable;
-    document.getElementById("resume-button").disabled = true; // Always keep resume disabled initially
-    document.getElementById("pass-button").disabled = !enable;
-    document.getElementById("guess-button").disabled = !enable;
-    document.querySelectorAll(".guess-input").forEach(input => input.disabled = !enable);
-}
+let previousGuesses = new Set();
+let highScores = [];
+let playerName = '';
+let currentPlayerHistory = [];
 
 function generateSecretNumber() {
     let digits = [];
@@ -20,83 +15,85 @@ function generateSecretNumber() {
             digits.push(newDigit);
         }
     }
-    console.log("Secret Number:", digits.join("")); // For debugging
     return digits.join("");
 }
 
 function startGame() {
-    if (!timerRunning) {
-        enableGameControls(true);
-        startTime = new Date();
-        timer = setInterval(updateTime, 1000);
-        timerRunning = true;
-        document.getElementById("start-button").disabled = true;
+    enableGameControls(true);
+    document.getElementById("start-button").disabled = true;
+    document.getElementById("pass-button").disabled = false;
+    startTime = new Date();
+    timer = setInterval(updateTimer, 1000);
+    timerRunning = true;
+}
+
+function updateTimer() {
+    if (timeRemaining <= 0) {
+        clearInterval(timer);
+        alert("Time's up! The number was " + secretNumber);
+        resetGame();
+        return;
     }
+
+    timeRemaining--;
+    let minutes = Math.floor(timeRemaining / 60);
+    let seconds = timeRemaining % 60;
+    document.getElementById("time-remaining").innerText = `Time Remaining: ${minutes}:${seconds.toString().padStart(2, '0')}`;
 }
 
 function pauseTimer() {
     clearInterval(timer);
     timerRunning = false;
+    setGameInteractions(false);
     document.getElementById("resume-button").disabled = false;
-    setGuessingEnabled(false); // Disable guessing when the game is paused
 }
 
 function resumeTimer() {
     if (!timerRunning) {
-        startTime = new Date() - (new Date() - startTime); // Adjust start time
-        timer = setInterval(updateTime, 1000);
+        startTime = new Date() - (new Date() - startTime);
+        timer = setInterval(updateTimer, 1000);
         timerRunning = true;
+        setGameInteractions(true);
         document.getElementById("resume-button").disabled = true;
-        setGuessingEnabled(true); // Enable guessing when the game is resumed
     }
-}
-
-function setGuessingEnabled(enabled) {
-    document.getElementById("guess-button").disabled = !enabled;
-    document.querySelectorAll(".guess-input").forEach(input => input.disabled = !enabled);
 }
 
 function passGame() {
     clearInterval(timer);
-    alert("The number was " + secretNumber);
+    let finalScore = calculateScore();
+    updateHighScores(finalScore);
+    alert("The number was " + secretNumber + ". Your score: " + finalScore);
     resetGame();
 }
 
 function resetGame() {
-    // Reset the game state
-    secretNumber = generateSecretNumber(); // Generate a new secret number
-    previousGuesses.clear(); // Clear previous guesses
+    secretNumber = generateSecretNumber();
+    previousGuesses.clear();
     attempts = 0;
-    document.getElementById("attempts-made").innerText = "Attempts: 0";
-    document.getElementById("history").innerHTML = "";
-    document.getElementById("result").innerHTML = "";
-    document.getElementById("time-taken").innerText = "Time: 00:00";
+    timeRemaining = 10 * 60; // Reset the timer
     timerRunning = false;
-
-    // Reset input fields and disable controls
-    for (let i = 1; i <= 4; i++) {
-        document.getElementById(`guess${i}`).value = '';
-    }
+    document.getElementById("time-remaining").innerText = "Time Remaining: 10:00";
+    document.getElementById("attempts-made").innerText = "Attempts: 0";
+    document.getElementById("current-score").innerText = "Score: 0";
     enableGameControls(false);
-
-    // Enable start button for a new game
     document.getElementById("start-button").disabled = false;
+    document.getElementById("player-name").disabled = false;
 }
 
-function updateTime() {
-    let currentTime = new Date();
-    let timeDiff = currentTime - startTime;
-    let seconds = Math.floor(timeDiff / 1000);
-    let minutes = Math.floor(seconds / 60);
-    seconds = seconds % 60;
-    document.getElementById("time-taken").innerText = `Time: ${minutes}:${seconds.toString().padStart(2, '0')}`;
+function enableGameControls(enable) {
+    document.getElementById("pause-button").disabled = !enable;
+    document.getElementById("pass-button").disabled = !enable;
+    setGameInteractions(enable);
 }
 
-function updateHistory(guess, bulls, cows) {
-    let historyElement = document.getElementById("history");
-    let newEntry = document.createElement("div");
-    newEntry.innerText = `Guess: ${guess} - Bulls: ${bulls}, Cows: ${cows}`;
-    historyElement.prepend(newEntry);
+function setGameInteractions(enabled) {
+    document.getElementById("guess-button").disabled = !enabled;
+    document.querySelectorAll(".guess-input").forEach(input => input.disabled = !enabled);
+    document.getElementById("notes").disabled = !enabled;
+}
+
+function calculateScore() {
+    return attempts === 0 ? 0 : Math.round(100 / attempts);
 }
 
 function checkGuess() {
@@ -110,7 +107,6 @@ function checkGuess() {
         return;
     }
 
-    // Prevent repeating the same guess
     if (previousGuesses.has(guess)) {
         alert("You have already guessed this number. Try a different number.");
         return;
@@ -128,18 +124,79 @@ function checkGuess() {
 
     attempts++;
     document.getElementById("attempts-made").innerText = `Attempts: ${attempts}`;
-    document.getElementById("result").innerText = `Guess: ${guess} - Bulls: ${bulls}, Cows: ${cows}`;
+    document.getElementById("current-score").innerText = `Score: ${calculateScore()}`;
 
     updateHistory(guess, bulls, cows);
 
     if (bulls === 4) {
         clearInterval(timer);
-        alert("Congratulations! You've guessed the right number!");
+        let finalScore = calculateScore();
+        alert("Congratulations! You've guessed the right number! Your score: " + finalScore);
+        updateHighScores(finalScore);
         resetGame();
     }
 }
 
+function updateHistory(guess, bulls, cows) {
+    currentPlayerHistory.push({ guess, bulls, cows });
+    displayCurrentPlayerHistory();
+}
+
+function displayCurrentPlayerHistory() {
+    let historyElement = document.getElementById("history");
+    historyElement.innerHTML = "<h3>Current Player History</h3>";
+    currentPlayerHistory.forEach(entry => {
+        historyElement.innerHTML += `<div>${entry.guess}: Bulls - ${entry.bulls}, Cows - ${entry.cows}</div>`;
+    });
+}
+
+function submitName() {
+    let newName = document.getElementById("player-name").value;
+    if (newName.trim() === '') {
+        alert("Please enter a name.");
+        return;
+    }
+
+    if (playerName !== newName) {
+        playerName = newName;
+        currentPlayerHistory = [];
+        displayCurrentPlayerHistory();
+    }
+
+    alert("Name submitted successfully!");
+    document.getElementById("player-name").disabled = true;
+}
+
+function updateHighScores(finalScore) {
+    let highScoreEntry = {
+        name: playerName || "Anonymous",
+        score: finalScore,
+        time: formatTime(10 * 60 - timeRemaining),
+        attempts: attempts
+    };
+
+    highScores.push(highScoreEntry);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores = highScores.slice(0, 5);
+
+    displayHighScores();
+}
+
+function displayHighScores() {
+    let highScoresElement = document.getElementById("high-scores");
+    highScoresElement.innerHTML = "<h3>High Scores</h3>";
+    highScores.forEach(entry => {
+        highScoresElement.innerHTML += `<div>${entry.name}: Score - ${entry.score}, Time - ${entry.time}, Attempts - ${entry.attempts}</div>`;
+    });
+}
+
+function formatTime(timeInSeconds) {
+    let minutes = Math.floor(timeInSeconds / 60);
+    let seconds = timeInSeconds % 60;
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
+
 window.onload = () => {
-    enableGameControls(false); // Disable game controls initially
-    setGuessingEnabled(false); // Also disable guessing initially
+    enableGameControls(false);
+    setGameInteractions(false);
 };
